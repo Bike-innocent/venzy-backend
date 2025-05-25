758ca4d337ef021e4c72a1ad9467feb7846e32c6
bike-innocent <onyemaobichibuikeinnocent.com@gmail.com>
2025-05-25 19:41:02 +0100

    email adjusting

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,13 +1,18 @@
+
+
+
 @component('mail::message')
-# Welcome {{ $user->name }} 👋
+# Welcome to Venzy, {{ $user->name }}! 🛍️
 
 Thank you for registering with us!
 
-@component('mail::button', ['url' => url('/')])
-Visit Our Website
+At **Venzy**, you’ll discover the latest perfumes and men’s wear  to match your style and budget.
+
+@component('mail::button', ['url' => 'https://venzy.vercel.app/shop'])
+Start Shopping
 @endcomponent
 
-We’re glad to have you onboard.
+We’re excited to have you on board and can’t wait for you to explore what we have in store.
 
 Thanks,<br>
 {{ config('app.name') }}
