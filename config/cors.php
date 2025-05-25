bf23944eff3a632c594ae8c0ed365d09fbf91e08
bike-innocent <onyemaobichibuikeinnocent.com@gmail.com>
2025-05-25 11:47:08 +0100

    added auth logich

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,27 @@
-
 <?php
+
+// return [
+//     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+//     'allowed_origins' => ['*'],
+
+//     'allowed_methods' => ['*'],
+//     'allowed_origins_patterns' => [],
+//     'allowed_headers' => ['*'],
+//     'exposed_headers' => [],
+//     'max_age' => 0,
+//     'supports_credentials' => true,
+// ];
+
 
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-    'allowed_origins' => ['*'],
 
-   // 'allowed_origins' => ['http://innoshop.chibuikeinnocent.tech','http://www.innoshop.chibuikeinnocent.tech','https://innoshop.chibuikeinnocent.tech','https://www.innoshop.chibuikeinnocent.tech','http://localhost:5173'],
+    'allowed_origins' => [
+        'https://venzy.chibuikeinnocent.tech',
+        'https://venzy.vercel.app',
+        'https://locahost/5173',
+    ],
+
     'allowed_methods' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
